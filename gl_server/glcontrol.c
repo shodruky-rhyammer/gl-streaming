884cea36d481958b7edd51bad14850427e356eaa
ArtDev <45949002+artdeell@users.noreply.github.com>
2020-05-01 16:32:54 +0300

    EGL surface getters instead of creators.

--- gl_server/glcontrol.c
+++ gl_server/glcontrol.c
@@ -42,82 +42,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 void init_egl(graphics_context_t *gc)
 {
-  /*
-  EGLBoolean r;
-  EGLint num_config;
-
-  VC_RECT_T dst_rect;
-  VC_RECT_T src_rect;
-
-  static const EGLint fb_attrib[] = {
-    EGL_RED_SIZE, 8,
-    EGL_GREEN_SIZE, 8,
-    EGL_BLUE_SIZE, 8,
-    EGL_ALPHA_SIZE, 8,
-    EGL_DEPTH_SIZE, 16,
-    EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-    EGL_NONE };
-
-  static const EGLint context_attrib[] = {
-    EGL_CONTEXT_CLIENT_VERSION, 2,
-    EGL_NONE };
-
-  EGLConfig config;
-
-  bcm_host_init();
- */
   gc->display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
   assert(gc->display != EGL_NO_DISPLAY);
- /*
- 
-  r = eglInitialize(gc->display, NULL, NULL);
-  assert(EGL_FALSE != r);
-  check_gl_err();
-
-  r = eglChooseConfig(gc->display, fb_attrib, &config, 1, &num_config);
-  assert(EGL_FALSE != r);
-  check_gl_err();
-
-  r = eglBindAPI(EGL_OPENGL_ES_API);
-  assert(EGL_FALSE != r);
-  check_gl_err();
-
-  gc->context = eglCreateContext(gc->display, config, EGL_NO_CONTEXT, context_attrib);
-  assert(gc->context != EGL_NO_CONTEXT);
-  check_gl_err();
-  
-  int32_t ri = graphics_get_display_size(0, &gc->screen_width, &gc->screen_height);
-  assert(ri >= 0);
-
-  dst_rect.x = 0;
-  dst_rect.y = 0;
-  dst_rect.width = gc->screen_width;
-  dst_rect.height = gc->screen_height;
-
-  src_rect.x = 0;
-  src_rect.y = 0;
-  src_rect.width = gc->screen_width << 16;
-  src_rect.height = gc->screen_height << 16;
-  */
-  gc->d_display = vc_dispmanx_display_open(0);
-  gc->d_update = vc_dispmanx_update_start(0);
-  gc->d_element = vc_dispmanx_element_add(
-    gc->d_update, gc->d_display,
-    0, &dst_rect, 0,
-    &src_rect, DISPMANX_PROTECTION_NONE, 0 , 0, (DISPMANX_TRANSFORM_T)0);
-
-  gc->d_window.element = gc->d_element;
-  gc->d_window.width = gc->screen_width;
-  gc->d_window.height = gc->screen_height;
-  check_gl_err();
-
-  gc->surface = eglCreateWindowSurface(gc->display, config, &gc->d_window, NULL);
+  check_gl_err();  
+  gc->surface = eglGetCurrentSurface(EGL_DRAW);
   assert(gc->surface != EGL_NO_SURFACE);
   check_gl_err();
   
-  r = eglMakeCurrent(gc->display, gc->surface, gc->surface, gc->context);
-  assert(EGL_FALSE != r);
-  check_gl_err();
   
   
 }
