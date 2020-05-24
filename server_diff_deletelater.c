4120ba75baa6745612a4b1eff2e30c2961d9a612
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-05-24 18:39:44 +0700

    This did overwrite server.c from client to server

--- server_diff_deletelater.c
+++ server_diff_deletelater.c
@@ -37,12 +37,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 
 #include "server.h"
-#include "fastlog.h"
 
 
 #define TRUE 1
 #define FALSE 0
-
 
 void server_init(server_context_t *c)
 {
@@ -56,15 +54,15 @@ void server_init(server_context_t *c)
 
 void * server_thread(void * arg)
 {
-  server_thread_args_t * a = (server_thread_args_t *)arg;
+  server_context_t * a = (server_context_t *)arg;
   int quit = FALSE;
 
   while (quit == FALSE)
   {
-    char* pushptr = fifo_push_ptr_get(a->fifo);
+    char* pushptr = fifo_push_ptr_get(&a->fifo);
     if (pushptr == NULL)
     {
-      LOGW("FIFO full!");
+      printf("FIFO full!\n");
       usleep(a->sleep_usec);
     }
     else
@@ -72,10 +70,10 @@ void * server_thread(void * arg)
       int recive_size = recvfrom(a->sock_fd, pushptr, a->max_packet_size, 0, NULL, NULL);
       if (recive_size == -1)
       {
-        LOGE("Socket recvfrom Error.");
+        printf("Socket recvfrom Error.\n");
         quit = TRUE;
       }
-      fifo_push_ptr_next(a->fifo);
+      fifo_push_ptr_next(&a->fifo);
     }
   }
 
@@ -107,94 +105,66 @@ void set_max_mbps(server_context_t *c, unsigned int mbps)
 }
 
 
-void set_server_address_port(server_context_t *c, char * addr, uint16_t port)
+void set_address_port(server_context_t *c, char * addr, uint16_t port)
 {
-  strncpy(c->server_thread_arg.addr, addr, sizeof(c->server_thread_arg.addr));
-  c->server_thread_arg.port = port;
+  strncpy(c->addr, addr, sizeof(c->addr));
+  c->port = port;
 }
 
-
-void set_client_address_port(server_context_t *c, char * addr, uint16_t port)
+void set_bind_address_port(server_context_t *c, char * addr, uint16_t port)
 {
-  strncpy(c->popper_thread_arg.addr, addr, sizeof(c->popper_thread_arg.addr));
-  c->popper_thread_arg.port = port;
-}
-
-
-void set_client_user_context(server_context_t *c, void *ptr)
-{
-  c->popper_thread_arg.user_context_ptr = ptr;
+  strncpy(c->bind_addr, addr, sizeof(c->bind_addr));
+  c->bind_port = port;
 }
 
 
 void socket_open(server_context_t *c)
 {
-  c->server_thread_arg.sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (c->server_thread_arg.sock_fd == -1)
+  struct sockaddr_in sai;
+  c->sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (c->sock_fd == -1)
   {
-    LOGE("Server Socket Open Error.");
-    // exit(EXIT_FAILURE);
+    printf("Server Socket Open Error.\n");
+    exit(EXIT_FAILURE);
   }
 
-  c->popper_thread_arg.sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (c->popper_thread_arg.sock_fd == -1)
+  c->sai.sin_family = AF_INET;
+  c->sai.sin_port = htons(c->port);
+  c->sai.sin_addr.s_addr = inet_addr(c->addr);
+
+  sai.sin_family = AF_INET;
+  sai.sin_port = htons(c->bind_port);
+  sai.sin_addr.s_addr = inet_addr(c->bind_addr);
+
+  if (bind(c->sock_fd, (struct sockaddr *)&sai, sizeof(struct sockaddr_in)) == -1)
   {
-    LOGE("Client Socket Open Error.");
-    // exit(EXIT_FAILURE);
-  }
-
-  c->server_thread_arg.sai.sin_family = AF_INET;
-  c->server_thread_arg.sai.sin_port = htons(c->server_thread_arg.port);
-  c->server_thread_arg.sai.sin_addr.s_addr = inet_addr(c->server_thread_arg.addr);
-
-  c->popper_thread_arg.sai.sin_family = AF_INET;
-  c->popper_thread_arg.sai.sin_port = htons(c->popper_thread_arg.port);
-  c->popper_thread_arg.sai.sin_addr.s_addr = inet_addr(c->popper_thread_arg.addr);
-
-  if (bind(c->server_thread_arg.sock_fd, (struct sockaddr *)&c->server_thread_arg.sai, sizeof(struct sockaddr_in)) == -1)
-  {
-    LOGE("Socket Bind Error.");
-    // exit(EXIT_FAILURE);
+    printf("Socket Bind Error.\n");
+    exit(EXIT_FAILURE);
   }
 }
 
 
 void socket_close(server_context_t *c)
 {
-  close(c->server_thread_arg.sock_fd);
-  close(c->popper_thread_arg.sock_fd);
+  close(c->sock_fd);
 }
 
 
-void server_run(server_context_t *c, void *(*popper_thread)(void *))
+void *server_start(server_context_t *c)
 {
   fifo_init(&c->fifo, c->fifo_size_in_bits, c->fifo_packet_size_in_bits);
-  c->server_thread_arg.fifo = &c->fifo;
-  c->popper_thread_arg.fifo = &c->fifo;
-  c->server_thread_arg.max_packet_size = 1 << c->fifo_packet_size_in_bits;
-  c->popper_thread_arg.max_packet_size = 1 << c->fifo_packet_size_in_bits;
-  c->server_thread_arg.sleep_usec = c->sleep_usec;
-  c->popper_thread_arg.sleep_usec = c->sleep_usec;
-  c->server_thread_arg.max_mbps = c->max_mbps;
-  c->popper_thread_arg.max_mbps = c->max_mbps;
+  c->max_packet_size = 1 << c->fifo_packet_size_in_bits;
 
   socket_open(c);
 
-  pthread_create(&c->server_th, NULL, (void* (*)(void*))server_thread, &c->server_thread_arg);
-  pthread_create(&c->popper_th, NULL, popper_thread, &c->popper_thread_arg);
-  pthread_join(c->popper_th, NULL);
-  
-  // From https://github.com/tinmaniac/gl-streaming/blob/master/gl_server/server.c#L188
-#ifdef __ANDROID__
-  // this is wrong, but android has no pthread_cancel
-  // see stack overflow for a better solution that uses a SIGUSR1 handler
-  // that I don't have time to implement right now
-  // http://stackoverflow.com/questions/4610086/pthread-cancel-alternatives-in-android-ndk
-  // pthread_kill(c->server_th, SIGUSR1);
-#else
-  pthread_cancel(c->server_th);
-#endif
-  
-  socket_close(c);
-  fifo_delete(&c->fifo);
+  pthread_create(&c->server_th, NULL, (void* (*)(void*))server_thread, c);
+  return c;
+}
+
+void server_stop(server_context_t *c)
+{
+    pthread_cancel(c->server_th);
+
+    socket_close(c);
+    fifo_delete(&c->fifo);
 }
