816869b23f53c92b457a9cf893aa7a62728e1cea
khanhduytran0 <khanhduytran032@gmail.com>
2020-05-31 20:17:08 +0700

    [Client] now can custom server IP by setting variable GLS_SERVER_IP.

--- gl_client/glsshared.c
+++ gl_client/glsshared.c
@@ -20,8 +20,17 @@ void gls_initshared()
     char his_ip[GLS_STRING_SIZE_PLUS];
     uint16_t my_port = 18146;
     uint16_t his_port = 18145;
+	
+	const char* env_serverIp = getenv("GLS_SERVER_IP");
+	
+	if (env_serverIp == NULL) {
+		env_serverIp = "127.0.0.1";
+	} else {
+		printf("GLS_SERVER_IP is set to %s\n", env_serverIp);
+	}
+	
     strncpy(my_ip, "127.0.0.1", GLS_STRING_SIZE);
-    strncpy(his_ip, "127.0.0.1", GLS_STRING_SIZE);
+    strncpy(his_ip, env_serverIp, GLS_STRING_SIZE);
     server_init(&sc);
     set_bind_address_port(&sc, my_ip, my_port);
     set_address_port(&sc, his_ip, his_port);
